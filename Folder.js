1aee093aaa45d68e6e9c1705142c7f6e43ef7501
Isabella Bertolo dos Santos <125597510+IsabellaBertolo@users.noreply.github.com>
2024-02-15 14:35:39 -0300

    Update and rename footer.js to Folder.js

---
+++ Folder.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const Rodape = () => {
+    return (
+        <View style={styles.footer}>
+            <Text style={styles.text}>Isabella Bertolo dos Santos </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    footer: {
+        backgroundColor: "#FFC0CB", // Rosa claro
+        padding: 10,
+        alignItems: "center",
+    },
+    text: {
+        color: "#FF69B4", // Rosa médio
+        fontSize: 14,
+    },
+});
+
+export default Rodape;
